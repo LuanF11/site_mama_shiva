5e9ea014803c3e097de6d9fdb15f58c4fdd87c4d
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-03-08 16:35:19 -0300

    adicionando os arquivos iniciais e criando index para controller, route e view

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const os = require('os');
+const ifaces = os.networkInterfaces();
+
+// Procura o primeiro endereço IPv4 não interno
+let ipAddress = null;
+Object.keys(ifaces).forEach((ifname) => {
+  ifaces[ifname].forEach((iface) => {
+    if (iface.family === 'IPv4' && !iface.internal) {
+      ipAddress = iface.address;
+      return;
+    }
+  });
+  if (ipAddress) {
+    return;
+  }
+});
+
+const indexRouter = require('./routes/indexRoute');
+
+// Configurando o EJS como view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Rotas
+app.use('/', indexRouter);
+
+// Inicializando o servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${ipAddress}:${PORT}`));
---
+++ controllers/indexController.js
@@ -0,0 +1,9 @@
+const indexController = {
+
+    // Renderiza a página principal
+    index(req, res) {
+      res.render('index');
+    }
+  };
+  
+module.exports = indexController;
