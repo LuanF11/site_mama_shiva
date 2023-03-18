c15b16d7743ac00a6a53011e46f820dc76555da7
caioalmeida08 <araujo.caio08@aluno.ifce.edu.br>
2023-03-18 12:36:00 -0300

    corriji a identação do arquivo

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,16 @@
 const indexController = {
 
-    // Renderiza a página principal
-    index(req, res) {
-      res.render('index', {
-        cssList : [
-          "css/reset.css",
-          "css/fonts.css",
-          "css/utils.css",
-          "css/indexCss.css",
-        ]
-      });
-    }
-  };
-  
+  // Renderiza a página principal
+  index(req, res) {
+    res.render('index', {
+      cssList: [
+        "css/reset.css",
+        "css/fonts.css",
+        "css/utils.css",
+        "css/indexCss.css",
+      ]
+    });
+  }
+};
+
 module.exports = indexController;
